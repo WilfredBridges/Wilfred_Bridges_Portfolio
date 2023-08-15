6ca1a102e7df08ce7966308d9252541223fc11af
Wilfred <wbridges905@gmail.com>
2023-08-15 23:20:29 +0100

    about me section set up

--- wilfred-portfolio/src/App.js
+++ wilfred-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header'
+import AboutMe from './components/aboutme/AboutMe'
 
 function App() {
   return (
@@ -8,6 +9,9 @@ function App() {
       <header className="App-header">
       <Header />
       </header>
+      <div classname='about'>
+      <AboutMe />
+      </div>
     </div>
   );
 }
---
+++ wilfred-portfolio/src/components/aboutme/AboutMe.js
@@ -0,0 +1,19 @@
+// components/AboutMe.js
+import React from 'react';
+import './AboutMe.css';
+
+const AboutMe = () => {
+  return (
+    <section id="about" className="about">
+      <div className="about-content">
+        <h2>About Me</h2>
+        <p>
+          Hi, I'm [Your Name]. I'm a passionate web developer with a strong interest in creating interactive and user-friendly web applications.
+          [Add more about yourself and your journey in web development.]
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default AboutMe;
